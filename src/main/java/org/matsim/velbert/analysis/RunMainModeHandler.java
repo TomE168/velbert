1beb8543c0f76b9658c267d2672a4c01b78597ab
Affemakjato <32954016+Affemakjato@users.noreply.github.com>
2021-05-27 12:47:03 +0200

    add analysis folder and mainModeHandler

---
+++ src/main/java/org/matsim/velbert/analysis/RunMainModeHandler.java
@@ -0,0 +1,43 @@
+package org.matsim.velbert.analysis;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.matsim.core.events.EventsUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RunMainModeHandler {
+
+    public static void main(String[] args) {
+
+        var manager = EventsUtils.createEventsManager();
+        var handler = new MainModeHandler();
+        manager.addHandler(handler);
+        EventsUtils.readEvents(manager, "C:\\Users\\tekuh\\Downloads\\...");
+
+        var personTrips = handler.getPersonTrips();
+        var modes = personTrips.values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toMap(mode -> mode, mode -> 1, Integer::sum));
+
+        var totalTrips = modes.values().stream()
+                .mapToDouble(d -> d)
+                .sum();
+
+        try (var writer = Files.newBufferedWriter(Paths.get("C:\\Users\\tekuh\\Desktop\\modes.csv")); var printer = CSVFormat.DEFAULT.withDelimiter(',').withHeader("Mode", "Count", "Share").print(writer)) {
+
+            for (var entry : modes.entrySet()) {
+                printer.printRecord(entry.getKey(), entry.getValue(), entry.getValue() / totalTrips);
+            }
+
+            printer.printRecord("total", totalTrips, 1.0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
